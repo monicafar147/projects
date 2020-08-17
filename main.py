730a9bdbdab13159dd2bbf304aac4af8d0a8f3e0
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-08-17 12:06:40 +0200

    Update main.py
    added some framework

--- main.py
+++ main.py
@@ -6,15 +6,52 @@ import pandas as pd
 
 # App declaration
 def main():
-    page_options = ["Home","About me"]
+    page_options = ["Home","About me", "Projects"]
 
-    page_selection = st.sidebar.selectbox("Choose Option", page_options)
+    page_selection = st.sidebar.selectbox("Pages", page_options)
     if page_selection == "Home":
         # Header contents
-        st.title('#Home')
-        
+        st.title('Home')
+        #Why data science
+       
     if page_selection == "About me":
         st.title("About me")
+        # soft skills
+        # community
+        # education
+
+    if page_selection == "Projects":
+        st.title("Projects")
+        option = st.sidebar.radio(
+            "Categories",
+            ("NLP",
+             "Recommender systems",
+             "Logistic optimisation",
+             )
+            )
+
+        if option == "NLP":
+            # NLP projects
+            st.header('NLP')
+            st.info('''Natural Language Processing is taking the data science world by storm.
+                    Analysing language can provide valuable business market value and Africa can
+                    benefit from research in NLP to improve education.''')
+            st.checkbox('Climate change tweet classifier')
+            st.checkbox('Vaccine tweet classifier')
+
+        if option == "Recommender systems":
+            # Movie recommender
+            st.header('Recommender systems')
+            st.info('''Recommender systems help users with choice paralysis to find content that
+                    the user might want. Streaming platforms such as Netflix changed the way that
+                    we view movies and series and by improving their recommender system, Netflix 
+                    can save on marketing costs. Recommender systems are also useful tools in 
+                    recruiting portals.''')
+            st.checkbox('IMDb movie recommender')
+        if option == "Logistic optimisation":
+            st.header('Logistic optimisation')
+
 
 if __name__ == '__main__':
     main()
+
